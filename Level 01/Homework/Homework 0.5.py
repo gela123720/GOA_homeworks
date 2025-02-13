2fb81f289ead33dbc19034eade779c7b01098e93
gela123720 <liongela0@gmail.com>
2025-02-13 18:34:28 +0400

    commit

--- Level 01/Homework/Homework 0.5.py
+++
@@ -1,9 +0,0 @@
-name = "Gela"
-surname = "Lomidze"
-age = 13
-height = 171.4
-knows_programming = True
-knows_swimming = True
-is_loud = False
-
-print("my name is" + " " + name + " " + surname + " " + "im" + " " + str(age) + " years old" + " " "im " + str(height) + " meters " + "i know programming and i know how to swim and im not loud" )
---
+++ Level 01/Homework/Homework 1.py
@@ -0,0 +1,8 @@
+name = "Gela"
+surname = "Lomidze"
+age = 13
+height = 171.4
+knows_programming = True
+knows_swimming = True
+
+print("my name is" + " " + name + " " + surname + " " + "im" + " " + str(age) + " years old" + " " "im " + str(height) + " Centimeters" + "i know programming and i know how to swim" )
