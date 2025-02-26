bb707d8bb81bf4a668370dcf429315d9fc6a4cac
gela123720 <liongela0@gmail.com>
2025-02-26 17:25:26 +0400

    commit

---
+++ Level 06/Homework/Homework 06.1.py
@@ -0,0 +1,10 @@
+surname = "Lomidze"
+print(surname)
+surname = "gogishvili"
+print(surname)
+surname = "begoidze"
+print(surname)
+surname = "sanadze"
+print(surname)
+surname = "Lomsadze"
+print(surname)
