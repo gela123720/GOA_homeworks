423c8bc0fe324d7d5a29a89b6dc4715d3d87946c
gela123720 <liongela0@gmail.com>
2025-03-25 19:17:38 +0400

    commit

--- Level 12/Homework/Homework 12.3.py
+++ Level 12/Homework/Homework 12.3.py
@@ -1,15 +1,15 @@
-#explicitly არის როცა თვითონ გარდაიქმნება რაღაცა რაღაცად
-#implicitly არის როცა ჩვენ გარდავქმნით რაღაცას სხვა რამედ
+#implicitly არის როცა თვითონ გარდაიქმნება რაღაცა რაღაცად
+#explicitly არის როცა ჩვენ გარდავქმნით რაღაცას სხვა რამედ
 
 #მაგალითად
 
-a = float(10) #ეს არის implicitly
-b = 4/2 #ეს არის explicitly
+a = float(10) #ეს არის explicitly
+b = 4/2 #ეს არის implicitly
 
 #ან
 
-c = str(50) #ეს არის implicitly
-d = "gela" #ეს არის explicitly
+c = str(50) #ეს არის explicitly
+d = "gela" #ეს არის implicitly
 
-e = int("100") #ეს არის implicitly
-f = 100*5 #ეს არის explicitly
+e = int("100") #ეს არის explicitly
+f = 100*5 #ეს არის implicitly
