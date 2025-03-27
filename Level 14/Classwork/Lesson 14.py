7b173fad5ad5d27cac539d90bea30905c839e152
gela123720 <liongela0@gmail.com>
2025-03-27 19:19:02 +0400

    commit

---
+++ Level 14/Classwork/Lesson 14.py
@@ -0,0 +1,36 @@
+#1)
+a = 10
+b = 20
+c = 30
+d = 40
+
+print(a > b)
+print(c > b)
+print(d > a)
+
+print(c < b)
+print(b < a)
+print(a < d)
+
+print(d <= a)
+print(a <= b)
+print(b <= c)
+
+print(a >= c)
+print(c >= b)
+print(b >= c)
+
+print(c == c)
+print(b == d)
+print(d == a)
+
+#2)
+
+name1 = "gela"
+
+name2 = input("Enter your name:")
+
+print(name1 == name2)
+
+
+
---
+++ Level 14/Homework/Homework 14.1.py
@@ -0,0 +1,14 @@
+Email1 = input("enter your email:")
+username1= input("enter your username:")
+password1 = input("enter your password:")
+
+print("you created a new account! now login into your new account")
+
+Email2 = input("enter your email:")
+username2 = input("enter your username:")
+password2 = input("enter your password:")
+
+print(Email1 == Email2)
+print(username1 == username2)
+print(password1 == password2)
+
