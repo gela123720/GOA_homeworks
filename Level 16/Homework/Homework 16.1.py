f5c1bfb32ae128993067e9e706924f22efcc6313
gela123720 <liongela0@gmail.com>
2025-04-05 12:07:15 +0400

    commit

---
+++ Level 16/Homework/Homework 16.1.py
@@ -0,0 +1,18 @@
+#control flow არის თანმიმდევრობა, რომლითაც ინდივიდუალური განცხადებები, ინსტრუქციები ან ფუნქციის გამოძახებები შესრულდება-
+#control flow რეგულირდება პირობითი განცხადებებით და ფუნქციის გამოძახებით.
+
+if 20>10: print(True)
+
+else: print(False)
+
+#
+
+if 20<15:print(True)
+
+else:print(False)
+
+#
+
+if 20==20:print(True)
+
+else:print(False)
---
+++ Level 16/Homework/Homework 16.2.py
@@ -0,0 +1,10 @@
+name = "Gela"
+password = 1234
+
+print("Please Enter Password To log in in account")
+
+password1 = int(input("Enter your password:"))
+
+if password == password1:print("password is correct!")
+
+else:print("incorrect password, please try again")
