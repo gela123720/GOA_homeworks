0e7def07e02b54c4610dddc3d33fcb6320c231e3
gela123720 <liongela0@gmail.com>
2025-04-21 22:41:33 +0400

    commit

---
+++ Level 20/Homework/Homework 20.4.py
@@ -0,0 +1,10 @@
+#4) მომხმარებელს შემოატანინეთ 5 რიცხვი for ციკლის გამოყენებით და დაბეჭდეთ მათი საშუალო არითმეტიკული.
+
+sum = 0
+
+for i in range(5):
+    num1 = int(input("please enter a number:"))
+    sum += num1
+
+print(sum / 5)
+
