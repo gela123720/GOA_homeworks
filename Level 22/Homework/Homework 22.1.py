59702b6c04b4801f51bdb55b36f6b2af1c866efe
gela123720 <liongela0@gmail.com>
2025-04-26 14:56:11 +0400

    commit

---
+++ Level 22/Homework/Homework 22.1.py
@@ -0,0 +1,4 @@
+for i in range(0,51,2):
+    print(i)
+
+print("there are 25 even numbers from 0 to 50")
---
+++ Level 22/Homework/Homework 22.5.py
@@ -0,0 +1,10 @@
+number = int(input("please enter any number:"))
+
+if number == 0:
+    print("number is equal to 0")
+
+elif number > 0:
+    print("this number is a positive number")
+
+elif number < 0:
+    print("this number is a negative number")
