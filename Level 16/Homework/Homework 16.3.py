d0be3aa9eabcf1b35b581187df06ea1d3467705e
gela123720 <liongela0@gmail.com>
2025-05-01 18:24:16 +0400

    commit

--- Level 16/Homework/Homework 16.3.py
+++ Level 16/Homework/Homework 16.3.py
@@ -1,5 +1,6 @@
 number = int(input("Please Enter a number:"))
 
-if (number % 2) == 0:print("number is even")
+if (number % 2) == 0:
+    print("number is even")
 
 else:print("number is odd")
---
+++ Level 24/Classwork/Lesson 24.py
@@ -0,0 +1,46 @@
+#1)
+number1 = 0
+
+while number1 < 10:
+    print(number1)
+    number1 = number1 + 1
+
+number2 = 0
+
+while number2 <= 10:
+    print(number2)
+    number2 = number2 + 1
+
+
+print()
+print()
+
+
+#2)
+number3 = 20
+
+while number3 < 100:
+    print(number3)
+    number3 = number3 + 1
+
+
+print()
+print()
+
+#3)
+number4 = 0
+
+while number4 < 20:
+    print(number4)
+    number4 = number4 + 2
+
+print()
+print()
+
+#4)
+number5 = 30
+
+while number5 < 50:
+    print(number5)
+    number5 = number5 + 4
+
